98ce2ccb0f10caae33c2f43c273c6de1087e3678
Rawnaa <rownaamesalam@gmail.com>
2025-11-26 11:23:50 -0800

    Add project files.

---
+++ DSC R2/Program.cs
@@ -0,0 +1,116 @@
+﻿namespace DSC_R2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //#region Q1
+            //Console.WriteLine("Enter a number");
+            //int num = int.Parse(Console.ReadLine());
+            //for (int i=1; i<= 12; i++)
+            //{
+            //    int result = num * i;
+            //}
+            //Console.WriteLine("result");
+
+            //#endregion 
+
+            //#region Q2
+            //int num = int.Parse(Console.ReadLine());
+            //int sum = 0;
+            //for( int i =0; i<= num;i++)
+            //{
+            //    sum += i;
+            //}
+            //Console.WriteLine(sum);
+            //#endregion
+
+            //#region Q3
+
+            //int countodd = 0;
+            //int counteven = 0;
+            //Console.WriteLine(" how many numbers they want to enter");
+            //int n = int.Parse(Console.ReadLine());
+            //for ( int i =1; i< n; i++)
+            //{
+            //    Console.WriteLine(i);
+            //    int num = int.Parse(Console.ReadLine());
+            //    if (n %2 == 0)
+            //    {
+            //        counteven++;
+            //    }
+            //    else
+            //    {
+            //        countodd++;
+            //    }
+
+            //}
+            //Console.WriteLine(counteven);
+            //Console.WriteLine(countodd);
+            //#endregion
+            //#region Q4
+            //int sum = 0;
+            //int num;
+            //for( int i =0; i<=10; i++)
+            //{
+            //    Console.WriteLine(i);
+            //    num = int.Parse(Console.ReadLine());
+
+            //    if( num<0)
+            //    {
+            //        continue;
+            //    }
+            //    if( num == 999)
+            //    {
+            //        break;
+            //    }
+            //    sum += num;
+            //}
+            //Console.WriteLine(sum);
+            //#endregion
+
+        //    #region Bonus
+
+        //    Console.Write("Enter the size  ");
+        //    int n = int.Parse(Console.ReadLine());
+        //    int[] nums = new int[n];
+
+        //    for (int i = 0; i < n; i++)
+        //    {
+        //        Console.Write( i );
+        //        nums[i] = int.Parse(Console.ReadLine());
+        //    }
+
+        //    Console.Write("Enter target ");
+        //    int target = int.Parse(Console.ReadLine());
+
+        //    int[] result = TwoSum(nums, target);
+
+        //    Console.WriteLine("[" + result[0] + "," + result[1] + "]");
+        //}
+
+        //static int[] TwoSum(int[] nums, int target)
+        //{
+        //    for (int i = 0; i < nums.Length; i++)
+        //    {
+        //        for (int j = i + 1; j < nums.Length; j++)
+        //        {
+        //            if (nums[i] + nums[j] == target)
+        //            {
+        //                return new int[] { i, j };
+        //            }
+        //        }
+        //    }
+        //    return new int[] { -1, -1 };
+
+
+
+        //    #endregion
+
+
+
+
+        }
+    }
+}
+
